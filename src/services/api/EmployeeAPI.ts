60e5d902a8a0c8121ce213423a8b4fd1f161945e
letruong <truongld@runsystem.net>
2023-04-17 16:53:44 +0700

    fix comment on Employee branch

--- src/services/api/EmployeeAPI.ts
+++ src/services/api/EmployeeAPI.ts
@@ -2,6 +2,9 @@ import { defineStore } from "pinia";
 import { axiosService } from "@/services/axios/axios";
 export const apiStore = defineStore("Employee", {
   actions: {
+    login(url: string, data: any) {
+      return axiosService.post(`${url}`, data);
+    },
     getAllEmployee(url: string, page: number, limit: number) {
       return axiosService.get(`/${url}?page=${page}&limit=${limit}`);
     },
